3ed971100bab5c1314b92cf41b69ef810c000cb5
ptrrr1 <limahenriquec@gmail.com>
2024-02-29 22:17:59 -0300

    was incorrect! now gui

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,20 @@ struct Planet {
 }
 
 impl Planet {
-    fn new(pos: Vec2) -> Planet {
+    fn new(pos: Vec2, mass: f32) -> Planet {
         Planet {
-            mass: 100.,
+            mass: mass,
             pos: pos,
             vel: Vec2::ZERO,
             acc: Vec2::ZERO,
         }
     }
 
-    fn update_acc(pos: Vec2, other_pos: Vec2, other_mass: f32) -> Vec2{
-        let direction = (other_pos - pos).normalize();
+    fn gravity_force(pos: Vec2, mass: f32, other_pos: Vec2, other_mass: f32) -> Vec2{
+        let direction = (other_pos - pos).clamp_length(1., 1.);
         let squared_dis = pos.distance_squared(other_pos);
 
-        return -1. * GRAVITY * other_mass * direction / squared_dis;
+        return GRAVITY * mass * other_mass * direction / squared_dis;
     }
 }
 
@@ -74,9 +74,9 @@ fn view(_app: &App, _model: &Model, _frame: Frame) {
     // Draw Planets
     for planet in &_model.planets {
         draw.ellipse()
-            .color(BLUEVIOLET)
+            .color(PLUM)
             .xy(planet.pos)
-            .wh(Vec2::ONE * planet.mass / 10.);
+            .wh(Vec2::ONE * (planet.mass / 3.).sqrt());
     }
 
     // Draw to frame
@@ -87,29 +87,29 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
     for planet in 0.._model.planets.len() {
         for other_planet in 0.._model.planets.len() {
             if planet != other_planet {
-                let pos = _model.planets[planet].pos;
-                let other_mass = _model.planets[other_planet].mass;
-                let other_pos = _model.planets[other_planet].pos;
-                let new_acc = Planet::update_acc(pos, other_pos, other_mass); // Calculate the new acc
+                let pos =_model.planets[planet].pos;
+                let mass =_model.planets[planet].mass;
+                let other_pos =_model.planets[other_planet].pos;
+                let other_mass =_model.planets[other_planet].mass;
 
-                _model.planets[other_planet].acc = new_acc;
+                _model.planets[planet].acc += Planet::gravity_force(pos, mass, other_pos, other_mass);
             }
         }
 
-        // Updates the position
-        // Idk if there's a better method other than creating a new variable
-        // Rust screams at me when i do otherwise
-        let new_acc = _model.planets[planet].acc.clamp_length_max(1.);
-        _model.planets[planet].vel += new_acc;
-        _model.planets[planet].vel.clamp_length_max(5.);
-        let new_vel = _model.planets[planet].vel;
-        _model.planets[planet].pos += new_vel;
+        _model.planets[planet].acc = _model.planets[planet].acc.normalize_or_zero();
+        let acc = _model.planets[planet].acc / _model.planets[planet].mass ;
+        _model.planets[planet].vel += acc;
+        let vel = _model.planets[planet].vel.clamp_length_max(5.);
+        _model.planets[planet].pos += vel;
+
     }
 }
 
 fn mouse_pressed(_app: &App, _model: &mut Model, _button: MouseButton) {
     match _button {
-        MouseButton::Left => { _model.planets.push( Planet::new(_app.mouse.position()) ) },
+        MouseButton::Left => { _model.planets.push( Planet::new(_app.mouse.position(), 27.) ) },
+
+        MouseButton::Middle => { _model.planets.push( Planet::new(_app.mouse.position(), 10_000.) ) },
 
         MouseButton::Right => { _model.planets.pop(); println!("Planets left: {}", _model.planets.len()); },
 
